f36bbc16cad55c98e88bec2d877087b7e8a3fa35
koebe1 <koe.be.pay@web.de>
2021-11-25 09:18:12 +0100

    counting valleys

--- algorithms.js
+++ algorithms.js
@@ -477,6 +477,39 @@ function pageCount(n, p) {
   }
 }
 
+// simple solution *not mine*
+function pageCount(n, p) {
+  /*
+    n: the number of pages in the book
+    p: the page number to turn to
+  */
+
+  const pageTurns = Math.floor(p / 2);
+  const totalTurns = Math.floor(n / 2);
+
+  /* Returns the total number of page turns it takes to get
+  to a page or how many it requires if starting from the back */
+
+  return Math.min(pageTurns, totalTurns - pageTurns);
+}
+
+// counting valleys
+function countingValleys(steps, path) {
+  // Write your code here
+
+  let sealevel = 0;
+  let valleys = 0;
+
+  for (let i = 0; i < path.length; i++) {
+    if (path[i] === "D" && sealevel === 0) {
+      valleys++;
+    }
+    path[i] === "D" ? sealevel-- : sealevel++;
+  }
+
+  return valleys;
+}
+
 // NEW CODE ABOVE
 // algorithmic challenges from the hackerrank 1 Month interview prep
 // week 2
